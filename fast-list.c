9024bf5e0bbb3e5c6d318378bc5838aff0f131a9
Michael Burge <michael.burge@rentrak.com>
2014-03-28 12:25:52 -0700

    Only print the filenames, and only print for files

--- fast-list.c
+++ fast-list.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
   struct linux_dirent *d;
   int bpos;
   char d_type;
-  
+
   fd = open(argc > 1 ? argv[1] : ".", O_RDONLY | O_DIRECTORY);
   if (fd == -1)
     handle_error("open");
@@ -35,28 +35,19 @@ int main(int argc, char *argv[])
     nread = syscall(SYS_getdents, fd, buf, BUF_SIZE);
     if (nread == -1)
       handle_error("getdents");
-    
+
     if (nread == 0)
       break;
-    
-    printf("--------------- nread=%d ---------------\n", nread);
-    printf("i-node#  file type  d_reclen  d_off   d_name\n");
-    for (bpos = 0; bpos < nread;) {
+
+    for (bpos = 0; bpos < nread; bpos += d->d_reclen) {
       d = (struct linux_dirent *) (buf + bpos);
-      printf("%8ld  ", d->d_ino);
       d_type = *(buf + bpos + d->d_reclen - 1);
-      printf("%-10s ", (d_type == DT_REG) ?  "regular" :
-	     (d_type == DT_DIR) ?  "directory" :
-	     (d_type == DT_FIFO) ? "FIFO" :
-	     (d_type == DT_SOCK) ? "socket" :
-	     (d_type == DT_LNK) ?  "symlink" :
-	     (d_type == DT_BLK) ?  "block dev" :
-	     (d_type == DT_CHR) ?  "char dev" : "???");
-      printf("%4d %10lld  %s\n", d->d_reclen,
-	     (long long) d->d_off, d->d_name);
-      bpos += d->d_reclen;
+      if (d_type != DT_REG)
+        continue;
+
+      printf("%s\n", d->d_name);
     }
   }
-  
+
   exit(EXIT_SUCCESS);
 }
